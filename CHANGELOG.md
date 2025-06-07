51e4f8e7c92a3d8752a6ae2e0b5453178df7f56b
TKundera <s096148@student.tu.kielce.pl>
2025-06-07 23:09:45 +0200

    Rozpoczęcie budowy głównej części analizatora

---
+++ CHANGELOG.md
@@ -0,0 +1,1 @@
+ - Test biblioteki console-log
---
+++ src/meteolizer.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+# Import the necessary packages
+from consolemenu import *
+from consolemenu.items import *
+import argparse
+
+def get_stats_from_file():
+    pass
+
+def get_graph_from_file():
+    pass
+
+def main():
+    def argparser():
+        parser = argparse.ArgumentParser(
+                prog='meteolizer',
+                description='Analizator danych meteorologicznych',
+                epilog='Wykonany przez: Wojciech Lis, Tomasz Kundera'
+                )
+
+        parser.add_argument('-f', '--file', help='Ścieżka pliku z danymi meteo w formacie CSV', type=str)
+        args = parser.parse_args()
+        parser.print_help()
+
+    def console_menu():
+        menu = ConsoleMenu(
+                "Meteolizer",
+                "Analizator danych meteorologicznych"
+                )
+
+        import_file = FunctionItem('Importuj plik CSV z danymi', input, [ 'Podaj ścieżkę pliku z danymi >  ' ])
+        stats_data = FunctionItem('Wylicz i wyświetl dane statystyczne importowanego pliku', get_stats_from_file)
+        plot_graph = FunctionItem('Wykreśl graf na podstawie danych z importowanego pliku', get_graph_from_file)
+
+        menu.append_item(import_file)
+        menu.append_item(stats_data)
+        menu.append_item(plot_graph)
+
+        menu.show()
+
+
+    # Wykonywanie
+    argparser()
+    console_menu()
+
+if __name__ == '__main__':
+    main()
