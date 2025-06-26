6c6c08fb6562d5e7af6b4fe74b9897e3e9df8c48
TKundera <s096148@student.tu.kielce.pl>
2025-06-26 16:06:34 +0200

    Kolejne zmiany

--- src/meteolizer.py
+++ src/meteolizer.py
@@ -6,26 +6,48 @@ from consolemenu.items import *
 import matplotlib.pyplot as plt
 import argparse
 import numpy as np
+import os
 
 import utils.data_generator as datagen
 import utils.analysis as asys
 import utils.models as models
 
 record = models.Record(list())
+def_path = os.getcwd()
+def_gen_filename = 'dane.csv'
 
 def generate_datafile(filepath):
-    data = datagen.file_data_generator(31, (20, 30))
+    data = datagen.file_data_generator(30, (20,30), (30,90), (0,20))
     datagen.data_writer(filepath, data)
 
 def datafile_generation_screen():
+    global def_path
+
     datafile_screen = Screen()
     datafile_screen.clear()
 
     datafile_screen.println('Generowanie pliku z danymi\n\n')
-    filepath = datafile_screen.input('Podaj ścieżkę plik CSV: ')
-    generate_datafile(filepath)
 
-    datafile_screen.println(f'Wygenerowano plik CSV i zapisano w {filepath}')
+    data_dir = datafile_screen.input('Podaj katalog pliku CSV: ')
+
+    if not os.path.exists(data_dir):
+        print(f'Ścieżka {data_dir} nie istnieje.')
+        print(f'Wybieram domyślną ścieżkę {def_path}')
+
+        data_dir = def_path
+
+    filename = ''
+    try:
+        filename = input('Podaj nazwę pliku CSV: ')
+
+        if not filename:
+            filename = def_gen_filename
+    except OSError as e:
+        print(f'Nie można utworzyć pliku {filename}!')
+        print(e)
+    else:
+        generate_datafile(data_dir + '/' + filename)
+
 
 def datafile_import_screen():
     global record 
@@ -71,11 +93,15 @@ def save_graph_screen():
 
 
     xp = [ x['pomiar'] for x in csv_data ]
-    yp = [ y['wartosc'] for y in csv_data ]
+    yp = [ y['temperatura'] for y in csv_data ]
+    wp = [ w['wilgotnosc'] for w in csv_data ]
+    pwp = [ pw['predkosc_wiatru'] for pw in csv_data ]
 
     filepath = save_screen.input('Wprowadź ścieżkę pliku obrazu z wykresem: ')
 
     plt.plot(xp, yp)
+    plt.plot(xp, wp)
+    plt.plot(xp, pwp)
     plt.title(f'{csv_station_name} | {csv_city_name}')
     plt.savefig(filepath)
 
@@ -93,15 +119,21 @@ def plot_sorted_graph_screen():
     plot_screen.println('Wykreśl graf w oparciu o posortowane zaimportowane dane\n\n')
 
     xp = [ x['pomiar'] for x in tmp_data ]
-    yp = [ y['wartosc'] for y in tmp_data ]
+    yp = [ y['temperatura'] for y in tmp_data ]
+    wp = [ w['wilgotnosc'] for w in tmp_data ]
+    pwp = [ pw['predkosc_wiatru'] for pw in tmp_data ]
+
 
     plot_screen.input('Naciśnij dowolny klawisz, by wykreślić wykres... ')
 
     plt.plot(xp, yp)
+    plt.plot(xp, wp)
+    plt.plot(xp, pwp)
     plt.grid(True)
     plt.title(f'{csv_station_name} | {csv_city_name}')
     plt.show()
     plt.close()
+
 
 def data_plot_screen():
     global record
@@ -115,14 +147,20 @@ def data_plot_screen():
     plot_screen.println('Wykreśl graf w oparciu o zaimportowane dane\n\n')
 
     xp = [ x['pomiar'] for x in csv_data ]
-    yp = [ y['wartosc'] for y in csv_data ]
+    yp = [ y['temperatura'] for y in csv_data ]
+    wp = [ w['wilgotnosc'] for w in csv_data ]
+    pwp = [ pw['predkosc_wiatru'] for pw in csv_data ]
 
     plot_screen.input('Naciśnij dowolny klawisz, by wykreślić wykres... ')
 
     plt.title(f'{csv_station_name} | {csv_city_name}')
-    plt.plot(xp, yp)
+    plt.plot(xp, yp, label='temperatura')
+    plt.plot(wp, label='wilgotnosc')
+    plt.plot(pwp, label='predkosc wiatru')
+
     plt.show()
     plt.close()
+
 
 def main():
     def argparser():
--- src/utils/data_generator.py
+++ src/utils/data_generator.py
@@ -4,31 +4,43 @@ import csv
 import random
 import datetime
 
-def row_generator(n, minimum, maximum):
+def row_generator(n, temp_range, humidity_range, wind_range):
     """
     Wygeneruj pojedynczy wiersz danych meteorologicznych
     """
+    temperature = random.randint(temp_range[0], temp_range[1])
+    humidity = generate_humidity(humidity_range[0], humidity_range[1])
+    wind_speed = generate_wind_speed(wind_range[0], wind_range[1])
 
-    # row = ( day, temp/hum)
-    # row = ( x, y )
-
-    value = random.randint(minimum, maximum)
     row = []
 
-    row.append(n+1)
-    row.append(value)
+    row.append(n+1)  # pomiar
+    row.append(temperature)
+    row.append(humidity)
+    row.append(wind_speed)
 
     return tuple(row)
 
-def file_data_generator(n, values):
+def generate_humidity(minimum, maximum):
+    """
+    Generuj losową wilgotność procentową
+    """
+    return random.randint(minimum, maximum)
+
+def generate_wind_speed(minimum, maximum):
+    """
+    Generuj siłę wiatru w m/s
+    """
+    return random.randint(minimum, maximum)
+
+def file_data_generator(n, temp_range, humidity_range, wind_range):
     """
     Utwórz zawartość pliku z danymi meteorologicznymi
     """
-
     data = []
 
     for i in range(n):
-        data_set = row_generator(i, values[0], values[1])
+        data_set = row_generator(i, temp_range, humidity_range, wind_range)
         data.append(data_set)
     return data
 
@@ -36,17 +48,15 @@ def data_writer(filepath, data):
     """
     Utwórz plik CSV i wpisz do niego dane meteorologiczne
     """
-
-    with open(filepath, 'w') as f:
+    with open(filepath, 'w', newline='') as f:
         writer = csv.writer(f)
-        writer.writerow(['pomiar','wartosc'])
+        writer.writerow(['pomiar', 'temperatura', 'wilgotnosc', 'predkosc_wiatru'])
         writer.writerows(data)
 
 def data_reader(filepath):
     """
     Odczytaj plik CSV i zwróć jego zawartość w liście
     """
-
     file_content = []
 
     with open(filepath, 'r') as f:
@@ -57,10 +67,15 @@ def data_reader(filepath):
 
     return file_content
 
-
 def main():
-    data = file_data_generator(10, (20, 30))
+    # Definiujemy zakresy dla temperatury, wilgotności i wiatru
+    temp_range = (20, 30)
+    humidity_range = (30, 90)   # wilgotność od 30% do 90%
+    wind_range = (0, 20)        # siła wiatru od 0 do 20 m/s
+
+    data = file_data_generator(10, temp_range, humidity_range, wind_range)
     data_writer('dane.csv', data)
 
 if __name__ == '__main__':
     main()
+
